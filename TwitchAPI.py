976c5ff46dcf5574ed68b9a88e4b6baee8cc0121
Marc Ghanime <marc.ghanime1@gmail.com>
2023-06-22 17:15:48 +0200

    Listen to whispers API

--- TwitchAPI.py
+++ TwitchAPI.py
@@ -1,17 +1,29 @@
-import requests, socket, queue, re, datetime, threading
+import requests, socket, queue, re, datetime, threading, asyncio
 from emoji import demojize
+from typing import Callable
+from twitchAPI.pubsub import PubSub
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.types import AuthScope
+from twitchAPI.helper import first
+from uuid import UUID
+
 
 CLIENT_ID = "0n2wpprouphucbxx48ctqamgbfoiwc"
 CLIENT_SECRET = "mdsmfgeknu8iypsmpou7b5xzxj98id"
 TWITCH_API_ACCESS_TOKEN = ""
+USER_TOKEN = "" #could also get at https://twitchapps.com/tmi/
 
-TWITCH_CHANNEL = 'nmplol'
+TWITCH_CHANNEL = 'skylibs'
 
 CHAT_SERVER = 'irc.chat.twitch.tv'
 CHAT_PORT = 6667
-CHAT_TOKEN = 'oauth:89tnhmu65sqaqx3vi3euhqa69w0d9p' # get at https://twitchapps.com/tmi/
 CHAT_NICKNAME = 'LibsGPT'
 CHAT_CHANNEL = f'#{TWITCH_CHANNEL}'
+
+twitch: Twitch = None
+pubsub: PubSub = None
+uuid: UUID = None
 
 sock = socket.socket()
 regex = r'^:(?P<user>[a-zA-Z0-9_]{4,25})!\1@\1\.tmi\.twitch\.tv PRIVMSG #(?P<channel>[a-zA-Z0-9_]{4,25}) :(?P<message>.+)$'
@@ -19,7 +31,7 @@ regex = r'^:(?P<user>[a-zA-Z0-9_]{4,25})!\1@\1\.tmi\.twitch\.tv PRIVMSG #(?P<cha
 
 def initialize_socket():
     sock.connect((CHAT_SERVER, CHAT_PORT))
-    sock.send(f"PASS {CHAT_TOKEN}\n".encode('utf-8'))
+    sock.send(f"PASS oauth:{USER_TOKEN}\n".encode('utf-8'))
     sock.send(f"NICK {CHAT_NICKNAME}\n".encode('utf-8'))
     sock.send(f"JOIN {CHAT_CHANNEL}\n".encode('utf-8'))
 
@@ -92,12 +104,51 @@ def get_stream_info():
     }
 
 
+def initialize_twitch_api(callback_whisper: Callable[[UUID, dict], None]):
+    print("Initializing Twitch API...")
+    asyncio.run(get_user_oauth_token())
+    get_twitch_oath_token()
+    initialize_socket()
+    asyncio.run(subscribe_to_whispers(callback_whisper))
+    print("Twitch API Initialized")
+
+
 def get_twitch_oath_token():
     global TWITCH_API_ACCESS_TOKEN
     url = 'https://id.twitch.tv/oauth2/token'
     params = {'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET, 'grant_type': 'client_credentials'}
     response = requests.post(url, params=params)
     TWITCH_API_ACCESS_TOKEN = response.json()["access_token"]
+
+
+async def get_user_oauth_token():
+    global twitch, USER_TOKEN
+    twitch = Twitch(CLIENT_ID, CLIENT_SECRET)
+    auth = UserAuthenticator(twitch, [AuthScope.WHISPERS_READ, AuthScope.CHAT_READ, AuthScope.CHAT_EDIT], force_verify=False)
+    USER_TOKEN, refresh_token = await auth.authenticate()
+    await twitch.set_user_authentication(USER_TOKEN, [AuthScope.WHISPERS_READ, AuthScope.CHAT_READ, AuthScope.CHAT_EDIT], refresh_token)
+
+
+def stop_listening_to_whispers():
+    asyncio.run(unsubscribe_from_whispers())
+
+
+async def subscribe_to_whispers(callback_whisper: Callable[[UUID, dict], None]):
+    global twitch, pubsub, uuid
+
+    user = await first(twitch.get_users(logins=[CHAT_NICKNAME]))
+    # starting up PubSub
+    pubsub = PubSub(twitch)
+    pubsub.start()
+    # you can either start listening before or after you started pubsub.
+    uuid = await pubsub.listen_whispers(user.id, callback_whisper)
+
+
+async def unsubscribe_from_whispers():
+    await pubsub.unlisten(uuid)
+    pubsub.stop()
+    await twitch.close()
+
 
 # def get_twitch_emotes():
 #     url = f"https://api.twitch.tv/helix/chat/emotes/global"

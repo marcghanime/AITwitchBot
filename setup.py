74b42ab20d83a9bc0ec1af6cd8292082c1e3a82e
Marc Ghanime <marc.ghanime1@gmail.com>
2023-07-27 12:22:06 +0200

    Removed legacy check

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import json
 from models import Config
 import dataclasses
-import pyaudio
 from faster_whisper import WhisperModel
 
 def main():
@@ -14,16 +13,6 @@ def main():
     except FileNotFoundError:
         with open ("config.json", "w") as outfile: json.dump(dataclasses.asdict(Config()), outfile, indent=4)
         print("config.json created. ")
-
-    dev_index = -1
-    p = pyaudio.PyAudio()
-    for i in range(p.get_device_count()):
-        dev = p.get_device_info_by_index(i)
-        if (dev['name'] == 'CABLE Output (VB-Audio Virtual' and dev['hostApi'] == 0):
-            dev_index = int(dev['index'])
-    
-    if not dev_index == -1:
-        print("WARNING: Virtual Audio Cable not found! Either install it or Change 'VIRTUAL_AUDIO_CABLE_NAME' in ChatAPI.py to the corresponding name in your system")
 
     print("Setup finished. Please fill out the config file before starting the bot.")
 
